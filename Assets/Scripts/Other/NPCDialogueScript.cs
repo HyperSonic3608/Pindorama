3993910ec969655705cacb4dd44194e7868bed38
Daniel-Henriq <137714763+Daniel-Henriq@users.noreply.github.com>
2025-12-02 18:35:54 -0300

    Update Dialogue

--- Assets/Scripts/Other/NPCDialogueScript.cs
+++ Assets/Scripts/Other/NPCDialogueScript.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class NPCDialogueScript : MonoBehaviour, IInteractable
 {
+    public NPCDialogue dialogueData;
+    public TMP_Text dialogueText;
+    private int dialogueIndex;
+    private bool isTyping;
+
+
     public GameObject dialoguePanel;
 
     private bool isDialogueActive;
@@ -17,19 +25,29 @@ public class NPCDialogueScript : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-            if (isDialogueActive)
-            {
-                StopDialogue();
-            }
-            else
-            {
-                StartDialogue();
-            }
+        if (dialogueData == null)
+        {
+            return;
+        }
 
+
+        if (!isDialogueActive)
+        {
+            StartDialogue();
+            
+        }
+        else
+        {
+            NextLine();
+            //StopDialogue();
+        }
     }
 
-    void StopDialogue()
+    public void StopDialogue()
     {
+        StopAllCoroutines();
+        dialogueText.SetText("");
+
         isDialogueActive = false;
         dialoguePanel.SetActive(false);
         playerCanMove = true;
@@ -39,6 +57,47 @@ public class NPCDialogueScript : MonoBehaviour, IInteractable
         isDialogueActive = true;
         dialoguePanel.SetActive(true);
         playerCanMove = false;
+        dialogueIndex = 0;
+
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if(++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            StopDialogue();
+        }
     }
 
     public bool GetPlayerCanMove()
